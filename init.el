b5b68d6f75a583f45ac53dff86c4a8267f99b761
Elvin Sindrilaru <esindril@cern.ch>
2016-10-07 15:14:08 +0200

    Update init.el file

--- init.el
+++ init.el
@@ -1,10 +1,10 @@
 ;; emacs kicker --- kick start emacs setup
 
 ;; disable loading of "default.el" at startup
- (setq inhibit-default-init t)
+(setq inhibit-default-init t)
 
 ;; enable visual feedback on selections
- (setq transient-mark-mode t)
+(setq transient-mark-mode t)
 
 ;; always end a file with a newline
 (setq require-final-newline 'query)
@@ -20,7 +20,7 @@
 (delete-selection-mode t)
 (transient-mark-mode t)
 
-(require 'cl)				; common lisp goodies, loop
+(require 'cl)                           ; common lisp goodies, loop
 
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
@@ -37,65 +37,59 @@
 ;; set local recipes
 (setq
  el-get-sources
- '((:name buffer-move			; have to add your own keys
-	  :after (progn
-		   (global-set-key (kbd "<C-S-up>")     'buf-move-up)
-		   (global-set-key (kbd "<C-S-down>")   'buf-move-down)
-		   (global-set-key (kbd "<C-S-left>")   'buf-move-left)
-		   (global-set-key (kbd "<C-S-right>")  'buf-move-right)))
+ '((:name buffer-move                   ; have to add your own keys
+          :after (progn
+                   (global-set-key (kbd "<C-S-up>")     'buf-move-up)
+                   (global-set-key (kbd "<C-S-down>")   'buf-move-down)
+                   (global-set-key (kbd "<C-S-left>")   'buf-move-left)
+                   (global-set-key (kbd "<C-S-right>")  'buf-move-right)))))
 
-   (:name smex				; a better (ido like) M-x
-	  :after (progn
-		   (setq smex-save-file "~/.emacs.d/.smex-items")
-		   (global-set-key (kbd "M-x") 'smex)
-		   (global-set-key (kbd "M-X") 'smex-major-mode-commands)))
+;;   (:name smex                          ; a better (ido like) M-x
+;;          :after (progn
+;;                   (setq smex-save-file "~/.emacs.d/.smex-items")
+;;                   (global-set-key (kbd "M-x") 'smex)
+;;                   (global-set-key (kbd "M-X") 'smex-major-mode-commands)))
 
-;;   (:name magit				; git meets emacs and a binding
-;;	  :after (progn
-;;		   (global-set-key (kbd "C-x C-z") 'magit-status)))
+;;   (:name magit                               ; git meets emacs and a binding
+;;        :after (progn
+;;                 (global-set-key (kbd "C-x C-z") 'magit-status)))
 
-   (:name flx-ido                       ; flx-ido
-	  :type git
-	  :url "git://github.com/lewang/flx.git"
-	  :features flx-ido
-	  :compile "flx-ido.el")))
-
-;;   (:name goto-last-change		; move pointer back to last change
-;;	  :after (progn
-;;		   ;; when using AZERTY keyboard, consider C-x C-_
-;;		   (global-set-key (kbd "C-x C-/") 'goto-last-change)))))
+;;   (:name goto-last-change            ; move pointer back to last change
+;;        :after (progn
+;;                 ;; when using AZERTY keyboard, consider C-x C-_
+;;                 (global-set-key (kbd "C-x C-/") 'goto-last-change)))))
 
 ;; now set our own packages
 (setq
  my:el-get-packages
  '(el-get                               ; el-get is self-hosting
    escreen                              ; screen for emacs, C-\ C-h
-   switch-window			; takes over C-x o
-   auto-complete			; complete as you type with overlays
-   yasnippet				; powerful snippet mode
+   switch-window                        ; takes over C-x o
+   auto-complete                        ; complete as you type with overlays
+   yasnippet                            ; powerful snippet mode
    git-modes                            ; git modes
    ido-vertical-mode                    ; ido vertical mode
    ido-ubiquitous                       ; ido everywhere
    projectile                           ; project wide interaction library
-   color-theme-solarized))		; nice looking emacs theme solarized
+   color-theme-solarized))
 
 ;; append the non standard one to the list above
 (setq my:el-get-packages
       (append my:el-get-packages
-	       (mapcar 'el-get-source-name el-get-sources)))
+               (mapcar 'el-get-source-name el-get-sources)))
 
 ;; install the new packages and init already installed packages
 (el-get 'sync my:el-get-packages)
 
 ;; enable the color-theme-solarized
-(load-theme  'solarized t)
+(load-theme 'solarized t)
 
 ;; on to the visual settings
-(setq inhibit-splash-screen t)		; no splash screen
-(line-number-mode 1)			; have line numbers and
-(column-number-mode 1)			; column numbers in the mode line
-;;(global-hl-line-mode)			; highlight current line
-(global-linum-mode 1)			; add line numbers on the left
+(setq inhibit-splash-screen t)          ; no splash screen
+(line-number-mode 1)                    ; have line numbers and
+(column-number-mode 1)                  ; column numbers in the mode line
+(global-hl-line-mode)                 ; highlight current line
+(global-linum-mode 1)                   ; add line numbers on the left
 (menu-bar-mode -1)                      ; no menu bar at the top
 
 ;; when mwheel present enable mouse-wheel-mode
@@ -103,8 +97,8 @@
   (mouse-wheel-mode t))                 ; support wheel mouse scrolling
 
 (when (display-graphic-p)
-  (scroll-bar-mode -1)			; no scroll bars
-  (tool-bar-mode   -1))			; no tool bar with icons
+  (scroll-bar-mode -1)                  ; no scroll bars
+  (tool-bar-mode   -1))                 ; no tool bar with icons
 
 (unless (string-match "apple-darwin" system-configuration)
   ;; on mac, there's always a menu bar drown, don't have it empty
@@ -157,7 +151,7 @@
 
 ;; use ido for minibuffer completion
 (require 'ido)
-(ido-mode t)
+(ido-mode 1)
 (setq ido-save-directory-list-file "~/.emacs.d/.ido.last")
 (setq ido-enable-flex-matching t)
 (setq ido-use-filename-at-point 'guess)
@@ -165,7 +159,6 @@
 (setq ido-default-buffer-method 'selected-window)
 
 ;; default key to switch buffer is C-x b, but that's not easy enough
-;;
 ;; when you do that, to kill emacs either close its frame from the window
 ;; manager or do M-x kill-emacs.  Don't need a nice shortcut for a once a
 ;; week (or day) action.
@@ -175,20 +168,15 @@
 
 ;; have vertical ido completion lists
 (setq ido-decorations
-      '("\n-> " "" "\n   " "\n   ..." "[" "]"
-	" [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]"))
+      '("\n-> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]"))
 
 ;; Vertical completion menu
 (require 'ido-vertical-mode)
-(ido-vertical-mode)
+(ido-vertical-mode 1)
 
 ;; ido support everywhere
 (require 'ido-ubiquitous)
-(ido-ubiquitous)
-
-;; enable flx-ido mode
-(require 'flx-ido)
-(flx-ido-mode 1)
+(ido-ubiquitous-mode 1)
 
 ;; C-x C-j opens dired with the cursor right on the file you're editing
 (require 'dired-x)
@@ -197,7 +185,7 @@
 (defun fullscreen ()
   (interactive)
   (set-frame-parameter nil 'fullscreen
-		       (if (frame-parameter nil 'fullscreen) nil 'fullboth)))
+                     (if (frame-parameter nil 'fullscreen) nil 'fullboth)))
 (global-set-key [f11] 'fullscreen)
 
 ;; enable projectile and caching
@@ -207,18 +195,24 @@
 ;; Enable formatting using Astyle in emacs
 (defun astyle-this-buffer (pmin pmax)
   (interactive "r")
-  (shell-command-on-region pmin pmax
-			   "astyle"                 ;; add options here...
-			   (current-buffer) t
-			   (get-buffer-create "*Astyle Errors*") t))
+  (shell-command-on-region pmin pmax "astyle"
+                         (current-buffer) t
+                         (get-buffer-create "*Astyle Errors*") t))
 
 ;; save the files that are opened for next time in the same directory
 (setq my-path default-directory)
-(if (file-exists-p
-     (concat my-path ".emacs.desktop"))
+(if (file-exists-p (concat my-path ".emacs.desktop"))
     (if (y-or-n-p "Read .emacs.desktop and add hook?")
-    (progn
-      (desktop-read my-path)
-      (add-hook 'kill-emacs-hook
-	    `(lambda ()
-	       (desktop-save ,my-path t))))))
+        (progn
+          (desktop-read my-path)
+          (add-hook 'kill-emacs-hook
+                    `(lambda ()
+                       (desktop-save ,my-path t))))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(frame-background-mode (quote dark)))
+
+
